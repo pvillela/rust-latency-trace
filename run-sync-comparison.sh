e297c30984ad3f53b79e68d6f091e09f9ca4c855
pvillela <pvillela@gmail.com>
2023-09-18 09:55:41 -0400

    Updated run-sync-comparison.sh to execute batches of 3 instead of 2, and with an additional arg value.

--- run-sync-comparison.sh
+++ run-sync-comparison.sh
@@ -4,10 +4,12 @@ OUTPUT=out/simple_sync_comparison.txt
 
 rm -f $OUTPUT
 
-for i in 0 200 400 800 1600 3200
+for i in 0 200 400 800 1600 3200 6400
 do
     cargo run -r --example simple_sync $i >> $OUTPUT
     cargo run -r --example simple_sync $i >> $OUTPUT
+    cargo run -r --example simple_sync $i >> $OUTPUT
+    cargo run -r --example simple_sync_x $i >> $OUTPUT
     cargo run -r --example simple_sync_x $i >> $OUTPUT
     cargo run -r --example simple_sync_x $i >> $OUTPUT
 done
