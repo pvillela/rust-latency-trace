336613d9a5fc0ed32d968af06dd26386b0482b0a
pvillela <pvillela@gmail.com>
2024-05-28 13:43:55 -0400

    Updated gendoc.sh cargo makedocs exclude args.

--- gen-doc.sh
+++ gen-doc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd latency_trace
-cargo makedocs -e log -e thread-local-collect -e tokio -e tracing-subscriber -e sha2 -e base64ct -i tracing-core
+cargo makedocs -e log -e thread_local_collect -e tokio -e tracing-subscriber -e sha2 -e base64ct -i tracing-core
 popd
 cargo doc -p latency_trace --no-deps
 
