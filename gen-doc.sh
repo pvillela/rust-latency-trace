9733a4b832beed84e0501ae544f2b5d00de87b3d
pvillela <pvillela@gmail.com>
2024-05-28 13:38:53 -0400

    Updated gendoc.sh to regenerate README.md and issue git status.

--- gen-doc.sh
+++ gen-doc.sh
@@ -4,3 +4,7 @@ pushd latency_trace
 cargo makedocs -e log -e thread-local-collect -e tokio -e tracing-subscriber -e sha2 -e base64ct -i tracing-core
 popd
 cargo doc -p latency_trace --no-deps
+
+cat readme0.md readme1.md readme2.md > README.md
+
+git status
