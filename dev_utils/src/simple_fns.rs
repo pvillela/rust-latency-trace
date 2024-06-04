c240469b7f8cf1f2fc54285d8a5a2bb555c4585c
pvillela <pvillela@gmail.com>
2024-06-04 10:49:35 -0400

    Updated benches to include nthreads.

--- dev_utils/src/simple_fns.rs
+++ dev_utils/src/simple_fns.rs
@@ -1,11 +1,12 @@
 //! Simple functions for tests and examples.
 
+use futures::future::join_all;
 use std::{hint::black_box, thread, time::Duration};
 use tracing::{instrument, trace_span, Instrument};
 
 /// Instrumented simple sync function
-#[instrument(level = "trace", skip(repeats, sleep_micros))]
-pub fn simple_fn_sync(repeats: usize, sleep_micros: u64) {
+#[instrument(level = "trace", skip(nrepeats, sleep_micros))]
+pub fn simple_fn_sync(nrepeats: usize, ntasks: usize, sleep_micros: u64) {
     #[instrument(level = "trace", skip(sleep_micros))]
     fn g_sync(i: usize, sleep_micros: u64) {
         // Simulated work
@@ -13,41 +14,55 @@ pub fn simple_fn_sync(repeats: usize, sleep_micros: u64) {
         black_box(i);
     }
 
-    for i in 0..repeats {
-        trace_span!("loop_body", foo = i % 2).in_scope(|| {
-            trace_span!("empty").in_scope(|| {
-                // Empty span used to show some of the tracing overhead.
-            });
+    let f = move || {
+        trace_span!("f").in_scope(|| {
+            for i in 0..nrepeats {
+                trace_span!("loop_body", foo = i % 2).in_scope(|| {
+                    trace_span!("empty").in_scope(|| {
+                        // Empty span used to show some of the tracing overhead.
+                    });
 
-            // Simulated work
-            thread::sleep(Duration::from_micros(sleep_micros * 3));
+                    // Simulated work
+                    thread::sleep(Duration::from_micros(sleep_micros * 3));
 
-            g_sync(i, sleep_micros);
+                    g_sync(i, sleep_micros);
+                });
+            }
         });
-    }
+    };
+
+    let hs = (0..ntasks).map(|_| thread::spawn(f)).collect::<Vec<_>>();
+    f();
+    hs.into_iter().for_each(|h| h.join().unwrap());
 }
 
 /// Uninstrumented simple sync function
-pub fn simple_fn_sync_un(repeats: usize, sleep_micros: u64) {
+pub fn simple_fn_sync_un(nrepeats: usize, ntasks: usize, sleep_micros: u64) {
     fn g_sync_un(i: usize, sleep_micros: u64) {
         // Simulated work
         thread::sleep(Duration::from_micros(sleep_micros * 2));
         black_box(i);
     }
 
-    for i in 0..repeats {
-        {
-            // Simulated work
-            thread::sleep(Duration::from_micros(sleep_micros * 3));
+    let f = move || {
+        for i in 0..nrepeats {
+            {
+                // Simulated work
+                thread::sleep(Duration::from_micros(sleep_micros * 3));
 
-            g_sync_un(i, sleep_micros);
-        };
-    }
+                g_sync_un(i, sleep_micros);
+            };
+        }
+    };
+
+    let hs = (0..ntasks).map(|_| thread::spawn(f)).collect::<Vec<_>>();
+    f();
+    hs.into_iter().for_each(|h| h.join().unwrap());
 }
 
 /// Instrumented simple async function
-#[instrument(level = "trace", skip(repeats, sleep_micros))]
-pub async fn simple_fn_async(repeats: usize, sleep_micros: u64) {
+#[instrument(level = "trace", skip(nrepeats, sleep_micros))]
+pub async fn simple_fn_async(nrepeats: usize, ntasks: usize, sleep_micros: u64) {
     #[instrument(level = "trace", skip(sleep_micros))]
     async fn g_async(i: usize, sleep_micros: u64) {
         // Simulated work
@@ -55,37 +70,54 @@ pub async fn simple_fn_async(repeats: usize, sleep_micros: u64) {
         black_box(i);
     }
 
-    for i in 0..repeats {
-        async {
-            trace_span!("empty").in_scope(|| {
-                // Empty span used to show some of the tracing overhead.
-            });
+    let f = || {
+        async move {
+            for i in 0..nrepeats {
+                async {
+                    trace_span!("empty").in_scope(|| {
+                        // Empty span used to show some of the tracing overhead.
+                    });
 
-            // Simulated work
-            thread::sleep(Duration::from_micros(sleep_micros * 3));
+                    // Simulated work
+                    thread::sleep(Duration::from_micros(sleep_micros * 3));
 
-            g_async(i, sleep_micros).await;
+                    g_async(i, sleep_micros).await;
+                }
+                .instrument(trace_span!("loop_body", foo = i % 2))
+                .await;
+            }
         }
-        .instrument(trace_span!("loop_body", foo = i % 2))
-        .await;
-    }
+        .instrument(trace_span!("f"))
+    };
+
+    let hs = (0..ntasks).map(|_| tokio::spawn(f())).collect::<Vec<_>>();
+    f().await;
+    join_all(hs).await.into_iter().for_each(|r| r.unwrap());
 }
 
 /// Uninstrumented simple async function
-pub async fn simple_fn_async_un(repeats: usize, sleep_micros: u64) {
+pub async fn simple_fn_async_un(nrepeats: usize, ntasks: usize, sleep_micros: u64) {
     async fn g_async_un(i: usize, sleep_micros: u64) {
         // Simulated work
         tokio::time::sleep(Duration::from_micros(sleep_micros * 2)).await;
         black_box(i);
     }
 
-    for i in 0..repeats {
-        async {
-            // Simulated work
-            thread::sleep(Duration::from_micros(sleep_micros * 3));
+    let f = || {
+        async move {
+            for i in 0..nrepeats {
+                async {
+                    // Simulated work
+                    thread::sleep(Duration::from_micros(sleep_micros * 3));
 
-            g_async_un(i, sleep_micros).await;
+                    g_async_un(i, sleep_micros).await;
+                }
+                .await;
+            }
         }
-        .await;
-    }
+    };
+
+    let hs = (0..ntasks).map(|_| tokio::spawn(f())).collect::<Vec<_>>();
+    f().await;
+    join_all(hs).await.into_iter().for_each(|r| r.unwrap());
 }
--- latency_trace/benches/benchmark.rs
+++ latency_trace/benches/benchmark.rs
@@ -9,7 +9,7 @@ use latency_trace::bench_support::{
 };
 use latency_trace::LatencyTrace;
 
-const REPEATS: usize = 100;
+const NREPEATS: usize = 100;
 const SLEEP_MICROS: u64 = 100;
 
 fn set_up_bench() {
@@ -17,48 +17,65 @@ fn set_up_bench() {
     measure_latencies1(lt);
 }
 
-fn sync_completion_bench() {
+fn sync_completion_bench(ntasks: usize) {
     let lt = LatencyTrace::default();
-    measure_latencies2(lt, || simple_fn_sync(REPEATS, SLEEP_MICROS));
+    measure_latencies2(lt, move || simple_fn_sync(NREPEATS, ntasks, SLEEP_MICROS));
 }
 
-fn sync_all_in_bench() {
+fn sync_all_in_bench(ntasks: usize) {
     let lt = LatencyTrace::default();
-    let timings = lt.measure_latencies(|| simple_fn_sync(REPEATS, SLEEP_MICROS));
+    let timings = lt.measure_latencies(move || simple_fn_sync(NREPEATS, ntasks, SLEEP_MICROS));
     black_box(timings);
 }
 
-fn sync_un_bench() {
-    simple_fn_sync_un(REPEATS, SLEEP_MICROS);
+fn sync_un_bench(ntasks: usize) {
+    simple_fn_sync_un(NREPEATS, ntasks, SLEEP_MICROS);
 }
 
-fn async_completion_bench() {
+fn async_completion_bench(ntasks: usize) {
     let lt = LatencyTrace::default();
-    measure_latencies2_tokio(lt, || simple_fn_async(REPEATS, SLEEP_MICROS));
+    measure_latencies2_tokio(lt, move || simple_fn_async(NREPEATS, ntasks, SLEEP_MICROS));
 }
 
-fn async_all_in_bench() {
+fn async_all_in_bench(ntasks: usize) {
     let lt = LatencyTrace::default();
-    let timings = lt.measure_latencies_tokio(|| simple_fn_async(REPEATS, SLEEP_MICROS));
+    let timings =
+        lt.measure_latencies_tokio(move || simple_fn_async(NREPEATS, ntasks, SLEEP_MICROS));
     black_box(timings);
 }
 
-fn async_un_bench() {
+fn async_un_bench(ntasks: usize) {
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .unwrap()
-        .block_on(simple_fn_async_un(REPEATS, SLEEP_MICROS));
+        .block_on(simple_fn_async_un(NREPEATS, ntasks, SLEEP_MICROS));
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("set-up", |b| b.iter(set_up_bench));
-    c.bench_function("sync_completion", |b| b.iter(sync_completion_bench));
-    c.bench_function("sync_all_in", |b| b.iter(sync_all_in_bench));
-    c.bench_function("sync_un", |b| b.iter(sync_un_bench));
-    c.bench_function("async_completion", |b| b.iter(async_completion_bench));
-    c.bench_function("async_all_in", |b| b.iter(async_all_in_bench));
-    c.bench_function("async_un", |b| b.iter(async_un_bench));
+    for ntasks in [0, 5] {
+        c.bench_function(&format!("set-up(ntasks={ntasks})"), |b| {
+            b.iter(set_up_bench)
+        });
+        c.bench_function(&format!("sync_completion(ntasks={ntasks})"), |b| {
+            b.iter(|| sync_completion_bench(ntasks))
+        });
+        c.bench_function(&format!("sync_all_in(ntasks={ntasks})"), |b| {
+            b.iter(|| sync_all_in_bench(ntasks))
+        });
+        c.bench_function(&format!("sync_un(ntasks={ntasks})"), |b| {
+            b.iter(|| sync_un_bench(ntasks))
+        });
+        c.bench_function(&format!("async_completion(ntasks={ntasks})"), |b| {
+            b.iter(|| async_completion_bench(ntasks))
+        });
+        c.bench_function(&format!("async_all_in(ntasks={ntasks})"), |b| {
+            b.iter(|| async_all_in_bench(ntasks))
+        });
+        c.bench_function(&format!("async_un(ntasks={ntasks})"), |b| {
+            b.iter(|| async_un_bench(ntasks))
+        });
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
